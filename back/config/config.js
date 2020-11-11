2bddf9ad614932baa967b7ab2449a4ff308b0dc6
sonteve <sds11609@gmail.com>
2020-11-11 15:58:12 +0900

    Feat: add  production config to server

--- back/config/config.js
+++ back/config/config.js
@@ -18,8 +18,8 @@ module.exports = {
   },
   production: {
     username: "root",
-    password: null,
-    database: "database_production",
+    password: process.env.DB_PASSWORD,
+    database: "react-nodebird",
     host: "127.0.0.1",
     dialect: "mysql",
   },
