41fbfd4c9ffd17cd5345bb17f3f8834aca1ef6a0
sonteve <sds11609@gmail.com>
2020-11-11 22:40:21 +0900

    Fix : devide Port prod & dev

--- front/sagas/index.ts
+++ front/sagas/index.ts
@@ -2,8 +2,10 @@ import { all } from "redux-saga/effects";
 import userSaga from "./user";
 import postSaga from "./post";
 import axios from "axios";
+import { host } from "../config/test";
 
-axios.defaults.baseURL = "http://api.sonteve.kr";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production" ? host : "http://localhost:3060";
 axios.defaults.withCredentials = true;
 export default function* rootSaga() {
   yield all([userSaga(), postSaga()]);
