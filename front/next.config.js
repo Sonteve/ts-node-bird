00140c0934c58c71dfa2ba873714dc086f2d9d54
sonteve <sds11609@gmail.com>
2020-11-11 12:42:55 +0900

    Feat : set next.config.js about bundle-analyzer

---
+++ front/next.config.js
@@ -0,0 +1,19 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true, // html,css,js를 .gz파일로 압축
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ],
+    };
+  },
+});
