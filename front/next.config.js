a8f5cd5f12209f29dd00822902dcaf9bf0fa2f8f
sonteve <sds11609@gmail.com>
2020-11-11 15:54:26 +0900

    Feat : remove analyizer immidiatly

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,4 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
+module.exports = {
   compress: true, // html,css,js를 .gz파일로 압축
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === "production";
@@ -16,4 +12,21 @@ module.exports = withBundleAnalyzer({
       ],
     };
   },
-});
+};
+
+/* module.exports = withBundleAnalyzer({
+    compress: true, // html,css,js를 .gz파일로 압축
+    webpack(config, { webpack }) {
+      const prod = process.env.NODE_ENV === "production";
+      return {
+        ...config,
+        mode: prod ? "production" : "development",
+        devtool: prod ? "hidden-source-map" : "eval",
+        plugins: [
+          ...config.plugins,
+          new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+        ],
+      };
+    },
+  });
+ */
