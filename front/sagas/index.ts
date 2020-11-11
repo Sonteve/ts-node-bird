5e9b0b898752efe3f49c1c2981a78550d7bfc1dd
sonteve <sds11609@gmail.com>
2020-11-11 17:26:03 +0900

    Feat : change from axios base url

--- front/sagas/index.ts
+++ front/sagas/index.ts
@@ -3,7 +3,7 @@ import userSaga from "./user";
 import postSaga from "./post";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3060";
+axios.defaults.baseURL = "http://3.34.190.114";
 axios.defaults.withCredentials = true;
 export default function* rootSaga() {
   yield all([userSaga(), postSaga()]);
